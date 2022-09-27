412642e17667a5e7b1ad8692647f2d239582f6e0
Ettella <ettella@hotmail.com>
2022-09-27 18:54:40 +0200

    osszegzes() function

--- js/tomb.js
+++ js/tomb.js
@@ -6,3 +6,14 @@ function tombKiir(){
         span.innerHTML += tomb[i] + ". ";
     }
 }
+
+function osszegzes(){
+    let osszeg = 0;
+    for(let i = 0; i< tomb.length; i++){
+        osszeg += tomb[i];
+    }
+
+    document.getElementById("osszeg").innerHTML = osszeg;
+}
+
+//házi: átlagoláselemek átlaga a tömbben
