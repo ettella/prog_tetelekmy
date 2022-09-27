0cbeba4bb268814b179b983b068252498286e89f
Ettella <ettella@hotmail.com>
2022-09-27 18:48:37 +0200

    tombKiir()  function

--- js/tomb.js
+++ js/tomb.js
@@ -1,6 +1,6 @@
 var tomb = [99, 18, 25, 3, 8, 110, 37, 26];
 
-function tombkiir(){
+function tombKiir(){
     let span = document.getElementById("tomb-elemei");
     for(let i =0; i < tomb.length; i++){
         span.innerHTML += tomb[i] + ". ";
